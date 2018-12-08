4f82e704ae9f48858e2db79cb9ce30b517fdf44a
Prajjwal-Arya <45590755+Prajjwal-Arya@users.noreply.github.com>
2018-12-08 20:40:10 +0530

    Update urls.py

--- mysite/photos/urls.py
+++ mysite/photos/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
     url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
     url(r'^library/$', views.showLibrary, name='library'),
+    url(r'^progress-bar-private-upload/$', views.ProgressBarPUploadView.as_view(), name='progress_bar_private_upload'),
 ]
