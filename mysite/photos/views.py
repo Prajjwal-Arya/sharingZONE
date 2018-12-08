46ac3be398d82ba8097d46a5aae9a367dde1bca2
Prajjwal-Arya <45590755+Prajjwal-Arya@users.noreply.github.com>
2018-12-08 20:37:01 +0530

    Update views.py

--- mysite/photos/views.py
+++ mysite/photos/views.py
@@ -5,7 +5,9 @@ from django.http import JsonResponse
 from django.views import View
 
 from .forms import PhotoForm
+from .forms import AtomForm
 from .models import Photo
+from .models import Atom
 
 
 class BasicUploadView(View):
@@ -29,7 +31,7 @@ class ProgressBarUploadView(View):
         return render(self.request, 'photos/progress_bar_upload/index.html', {'photos': photos_list})
 
     def post(self, request):
-        time.sleep(1)  # You don't need this line. This is just to delay the process so you can see the progress bar testing locally.
+        time.sleep(1)
         form = PhotoForm(self.request.POST, self.request.FILES)
         if form.is_valid():
             photo = form.save()
@@ -54,6 +56,7 @@ class DragAndDropUploadView(View):
         return JsonResponse(data)
 
 
+
 def clear_database(request):
     for photo in Photo.objects.all():
         photo.file.delete()
@@ -67,5 +70,28 @@ def showLibrary(request):
 
 
 def home(request):
-    documents = Document.objects.all()
-    return render(request, 'core/home.html', { 'documents': documents })
+    photos = Photo.objects.all()
+    return render(request, 'core/home.html',  {'photos': photos})
+
+
+class ProgressBarPUploadView(View):
+    def get(self, request):
+        atoms_list = Atom.objects.all()
+        return render(self.request, 'photos/progress_bar_private_upload/index.html', {'atoms': atoms_list})
+
+    def post(self, request):
+        time.sleep(1)
+        form = AtomForm(self.request.POST, self.request.FILES)
+        if form.is_valid():
+            atom = form.save()
+            data = {'is_valid': True, 'name': atom.file.name, 'url': atom.file.url}
+        else:
+            data = {'is_valid': False}
+        return JsonResponse(data)
+
+
+
+
+
+
+
