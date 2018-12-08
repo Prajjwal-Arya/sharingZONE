951f014f28e90b7e48fd0bcda4be7fd047a72679
Prajjwal-Arya <45590755+Prajjwal-Arya@users.noreply.github.com>
2018-12-08 20:41:05 +0530

    Update forms.py

--- mysite/photos/forms.py
+++ mysite/photos/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
 from .models import Photo
+from .models import Atom
 
 
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
+        fields = ('file', )
+
+
+class AtomForm(forms.ModelForm):
+    class Meta:
+        model = Atom
         fields = ('file', )
