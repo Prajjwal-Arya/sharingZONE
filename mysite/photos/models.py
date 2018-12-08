1f3ef28118f4e98d84c9058f4572faf3763cf2fd
Prajjwal-Arya <45590755+Prajjwal-Arya@users.noreply.github.com>
2018-12-08 20:40:39 +0530

    Update models.py

--- mysite/photos/models.py
+++ mysite/photos/models.py
@@ -5,5 +5,11 @@ from django.db import models
 
 class Photo(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to='photos/')
+    file = models.FileField(upload_to='public/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+class Atom(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    file = models.FileField(upload_to='private/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
